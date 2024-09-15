d522d18c445bd730012e21fdab027b5500733e48
Chris-Coleongco <christien.coleongco@gmail.com>
2024-09-15 09:33:38 -0600

    STOW ALLLL MY DOTFILES HEHEUHEUHEAHUHAEAS

---
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,24 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = { "html", "cssls", "pyright", "gopls", "jdtls", "clangd", "tsserver" }
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+lspconfig.rust_analyzer.setup {
+  -- Server-specific settings. See `:help lspconfig-setup`
+  settings = {
+    ["rust-analyzer"] = {},
+  },
+} -- need to add formatting bawegwj
