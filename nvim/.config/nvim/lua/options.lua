dc31aef89b87808866624637e3b001b6286916ae
Chris-Coleongco <christien.coleongco@gmail.com>
2024-09-15 09:33:38 -0600

    STOW ALLLL MY DOTFILES HEHEUHEUHEAHUHAEAS

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 20
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
