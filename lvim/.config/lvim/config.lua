00c4bb80ec3f89dda7059a8ebf14c486c66b324d
Francois-Coleongco <christien.coleongco@gmail.com>
2025-04-08 21:30:06 -0600

    changed polybar transparency. also i dunno i kinda like lunarvim

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -10,35 +10,36 @@ vim.g.mapleader = " "
 vim.opt.shiftwidth = 2
 vim.opt.clipboard = "unnamedplus"
 vim.opt.scrolloff = 20
-vim.opt.tabstop = 2 -- insert 2 spaces for a tab
+vim.opt.tabstop = 2           -- insert 2 spaces for a tab
 vim.opt.number = true
 vim.opt.relativenumber = true -- relative line numbers
-vim.opt.wrap = true -- wrap lines
+vim.opt.wrap = true           -- wrap lines
 
+lvim.format_on_save = true
 lvim.colorscheme = "poimandres"
 lvim.transparent_window = true
 
 lvim.plugins = {
--- Lua
+  -- Lua
 
-{ 
-  'olivercederborg/poimandres.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('poimandres').setup {
-      -- leave this setup function empty for default config
-      -- or refer to the configuration section
-      -- for configuration options
-    }
-  end,
+  {
+    'olivercederborg/poimandres.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('poimandres').setup {
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      }
+    end,
 
-  -- optionally set the colorscheme within lazy config
-  init = function()
-    vim.cmd("colorscheme poimandres")
-  end
+    -- optionally set the colorscheme within lazy config
+    init = function()
+      vim.cmd("colorscheme poimandres")
+    end
 
-},
+  },
   {
     'goolord/alpha-nvim',
   }
@@ -49,59 +50,59 @@ lvim.plugins = {
 local dashboard = require "alpha.themes.dashboard"
 
 local default_header = {
-    type = "text",
-    val = {
-[[⠛⠛⠛⠻⠿⠾⠿⠿⠿⠿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
-[[⠀⠀⠀⡀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠈⠉⠉⠙⠙⠛⠛⠛⠛⢛⠿⣿⠦⢴⠀⠀⠀⠀⠀⠨⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
-[[⠆⠼⠏⠛⠀⠀⠈⠀⠀⠀⠀⠀⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⢠⠜⢃⣤⣾⡿⠂⠇⠊⠀⢸⠒⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠙⠛⠋⠛⠛⠿⠿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿]],
-[[⠂⠚⠒⠂⠒⠀⠐⠠⠤⠤⠤⠠⠠⠤⠤⠠⢌⣤⣖⣲⣢⣀⣤⣾⡟⢻⢣⣾⣿⣿⣿⣿⣷⣴⣢⣿⢗⠀⠀⠀⠰⣲⠒⡒⠀⠀⠀⠄⠤⠤⡀⡠⢄⠀⠡⡀⣀⣀⡀⡀⠀⠀⠀⠀]],
-[[⡲⣢⠀⡐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⣅⣴⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠈⠢⣀⣔⣠⣤⣴⣧⣬⣴⣶⣧⣻⣶⣾⣮⣾⣾⣶⣾⣿⣼⣿]],
-[[⣶⣶⣶⣶⣶⣶⣶⣶⣤⣤⣤⣤⣤⣤⣴⣶⣶⣶⣴⣦⣤⣤⣠⡨⠘⢾⡿⠟⣿⣿⣿⣿⣿⠿⠿⠿⣿⡿⢧⠀⠀⣶⢸⢶⣿⡿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
-[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⡅⠀⣽⡇⠰⣦⣠⡟⠻⣇⠠⠶⠂⢸⣿⣤⠄⠀⠀⠈⣿⣿⣾⣶⣾⣿⣿⣷⣷⣿⣶⣶⣶⣶⣶⣶⣤⣤⣬⣭⣭]],
-[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡽⠀⠀⣿⣿⣿⣿⡿⡠⠀⢻⣿⣷⣶⣿⣿⡟⠀⠀⠀⠀⣱⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
-[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⢸⣿⣿⣿⡿⣾⣷⣾⣿⣿⠟⠛⠉⠁⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
-[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠘⣿⠹⣿⡿⣷⡽⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
-[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡫⣿⣿⣿⡃⣿⠀⠀⠀⠀⡀⠀⠀⡈⢐⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
-[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣯⢹⡴⠞⣇⠀⠘⠀⠈⠀⠂⡠⠈⠉⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
-[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢛⣯⣶⣿⢳⠛⠟⣛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠛⠿⠿⢿⠿⣿⡿⠿⢿⣿⢿⣿⣿⣿⢿⣿⣿]],
-[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⠏⣠⡞⠃⢀⠀⠀⠀⠀⠀⠁⠂⡀⠀⢀⠀⠐⠀⠠⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠈⠁⠀⠐⡀⠀]],
-[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⣴⣿⣿⣿⡿⢋⣱⡾⠋⠀⠀⠀⠁⠀⠀⠀⠀⢀⡬⠤⠛⠀⠉⠀⠀⠀⠈⠡⠀⠄⠈⢠⣀⡀⠀⠀⠀⡈⠀⠀⠀⠀⠀⠀]],
-[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⢼⣿⣿⣿⢿⣶⣿⠟⢠⡀⠁⠀⢀⠀⠺⠠⠠⠺⣫⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠈⢠⣾⣿⣷⣠⠀⠀⠀⠀⠀⠀⠀⠀]],
-[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠈⡿⣿⣿⢿⠿⡣⣷⣾⠄⠐⠀⠀⠐⡼⠶⠀⠌⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⠷⠺⠿⠿⠓⠚⠠⠐⠄⠀⠀⠀]],
-[[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣧⣤⣤⣼⣿⣹⢃⣴⣿⡾⢳⠊⠀⢀⠀⠐⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠁⠀⠀⠈]],
-    },
-    opts = {
-        position = "center",
-        hl = "Type",
-        -- wrap = "overflow";
-    },
+  type = "text",
+  val = {
+    [[⠛⠛⠛⠻⠿⠾⠿⠿⠿⠿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+    [[⠀⠀⠀⡀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠈⠉⠉⠙⠙⠛⠛⠛⠛⢛⠿⣿⠦⢴⠀⠀⠀⠀⠀⠨⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+    [[⠆⠼⠏⠛⠀⠀⠈⠀⠀⠀⠀⠀⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⢠⠜⢃⣤⣾⡿⠂⠇⠊⠀⢸⠒⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠙⠛⠋⠛⠛⠿⠿⠿⠿⠿⠿⢿⣿⣿⣿⣿⣿]],
+    [[⠂⠚⠒⠂⠒⠀⠐⠠⠤⠤⠤⠠⠠⠤⠤⠠⢌⣤⣖⣲⣢⣀⣤⣾⡟⢻⢣⣾⣿⣿⣿⣿⣷⣴⣢⣿⢗⠀⠀⠀⠰⣲⠒⡒⠀⠀⠀⠄⠤⠤⡀⡠⢄⠀⠡⡀⣀⣀⡀⡀⠀⠀⠀⠀]],
+    [[⡲⣢⠀⡐⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⣅⣴⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠈⠢⣀⣔⣠⣤⣴⣧⣬⣴⣶⣧⣻⣶⣾⣮⣾⣾⣶⣾⣿⣼⣿]],
+    [[⣶⣶⣶⣶⣶⣶⣶⣶⣤⣤⣤⣤⣤⣤⣴⣶⣶⣶⣴⣦⣤⣤⣠⡨⠘⢾⡿⠟⣿⣿⣿⣿⣿⠿⠿⠿⣿⡿⢧⠀⠀⣶⢸⢶⣿⡿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+    [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⡅⠀⣽⡇⠰⣦⣠⡟⠻⣇⠠⠶⠂⢸⣿⣤⠄⠀⠀⠈⣿⣿⣾⣶⣾⣿⣿⣷⣷⣿⣶⣶⣶⣶⣶⣶⣤⣤⣬⣭⣭]],
+    [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡽⠀⠀⣿⣿⣿⣿⡿⡠⠀⢻⣿⣷⣶⣿⣿⡟⠀⠀⠀⠀⣱⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+    [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⢸⣿⣿⣿⡿⣾⣷⣾⣿⣿⠟⠛⠉⠁⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+    [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠘⣿⠹⣿⡿⣷⡽⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+    [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡫⣿⣿⣿⡃⣿⠀⠀⠀⠀⡀⠀⠀⡈⢐⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+    [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣯⢹⡴⠞⣇⠀⠘⠀⠈⠀⠂⡠⠈⠉⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+    [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢛⣯⣶⣿⢳⠛⠟⣛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠛⠿⠿⢿⠿⣿⡿⠿⢿⣿⢿⣿⣿⣿⢿⣿⣿]],
+    [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⠏⣠⡞⠃⢀⠀⠀⠀⠀⠀⠁⠂⡀⠀⢀⠀⠐⠀⠠⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠈⠁⠀⠐⡀⠀]],
+    [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⣴⣿⣿⣿⡿⢋⣱⡾⠋⠀⠀⠀⠁⠀⠀⠀⠀⢀⡬⠤⠛⠀⠉⠀⠀⠀⠈⠡⠀⠄⠈⢠⣀⡀⠀⠀⠀⡈⠀⠀⠀⠀⠀⠀]],
+    [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⢼⣿⣿⣿⢿⣶⣿⠟⢠⡀⠁⠀⢀⠀⠺⠠⠠⠺⣫⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⠈⢠⣾⣿⣷⣠⠀⠀⠀⠀⠀⠀⠀⠀]],
+    [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠈⡿⣿⣿⢿⠿⡣⣷⣾⠄⠐⠀⠀⠐⡼⠶⠀⠌⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⠷⠺⠿⠿⠓⠚⠠⠐⠄⠀⠀⠀]],
+    [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣧⣤⣤⣼⣿⣹⢃⣴⣿⡾⢳⠊⠀⢀⠀⠐⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠁⠀⠀⠈]],
+  },
+  opts = {
+    position = "center",
+    hl = "Type",
+    -- wrap = "overflow";
+  },
 }
 
 local buttons = {
-    type = "group",
-    val = {
-        dashboard.button("SPC ff", "  Find File", ":Telescope find_files<CR>"),
-         { type = "padding", val = 1 },
-        dashboard.button("SPC e", "  New File", ":ene!<CR>"),
-         { type = "padding", val = 1 },
-        dashboard.button("SPC f r", "  Recently Used Files", ":Telescope oldfiles<CR>"),
-         { type = "padding", val = 1 },
-        dashboard.button("SPC gp", "  Greppy", ":Telescope live_grep<CR>"),
-    },
-    position = "center",
+  type = "group",
+  val = {
+    dashboard.button("SPC ff", "  Find File", ":Telescope find_files<CR>"),
+    { type = "padding", val = 1 },
+    dashboard.button("SPC e", "  New File", ":ene!<CR>"),
+    { type = "padding", val = 1 },
+    dashboard.button("SPC f r", "  Recently Used Files", ":Telescope oldfiles<CR>"),
+    { type = "padding", val = 1 },
+    dashboard.button("SPC gp", "  Greppy", ":Telescope live_grep<CR>"),
+  },
+  position = "center",
 }
 
 lvim.builtin.alpha.dashboard.config = {
-    layout = {
-         { type = "padding", val = 2 },
-         default_header,
-         { type = "padding", val = 2 },
-         buttons,
-    },
-    opts = {
-        margin = 5,
-        setup = function()
-            vim.cmd [[autocmd alpha_temp DirChanged * lua require('alpha').redraw()]]
-        end,
-    },
+  layout = {
+    { type = "padding", val = 2 },
+    default_header,
+    { type = "padding", val = 2 },
+    buttons,
+  },
+  opts = {
+    margin = 5,
+    setup = function()
+      vim.cmd [[autocmd alpha_temp DirChanged * lua require('alpha').redraw()]]
+    end,
+  },
 }
